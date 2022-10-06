c08c0aa5a5dd0b4c1da423b68fdaedcde3058690
Chris Rodgers <cj.rodgers@gmail.com>
2022-10-06 08:14:56 +0100

    starting validation - 1:30:05

---
+++ src/app/components/add-task/add-task.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.scss']
+})
+export class AddTaskComponent implements OnInit {
+  text: string;
+  day: string;
+  reminder: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    if (!this.text) {
+      alert('please add a task')
+    }
+  }
+}
